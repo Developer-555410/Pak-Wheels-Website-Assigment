31996dcb884db031823f93e34671b110703b04ce
Developer-555410 <muhsaeabb1995@gmail.com>
2024-10-29 18:57:45 +0500

    first commit

---
+++ app/NewCars/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Navbar from "../components/Navbar/Navbar"
+import React from 'react';
+import './NewCars.css';
+import Footer from "../components/Footer/Footer";
+
+const NewCars = () => {
+  return (
+    <div>
+     <Navbar/>
+     <a href="NewCars"></a>
+
+     <div className="new-cars">
+      <h1>Honda Civic 2024 Price in Pakistan, Images, Reviews & Specs</h1>
+      <img src="/civic.png" alt="Honda Civic 2024" />
+      <p>
+      Honda Civic 2024 offers a blend of reliability, fuel efficiency, and comfortable driving experience.
+      It's known for its spacious interior, advanced safety features, and smooth handling, making it a popular choice for daily commuting and family outings
+ </p>
+      <div className="price">
+        <p>PKR 98,000,000</p>
+        <div className="button-row">
+      <button>Make Payment</button>
+      <button>Request Bank Finance</button>
+      <button>Visit Place</button>
+      <button>Car Ispection</button>
+    </div>
+               
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <Footer/>
+    </div>
+    
+  );
+};
+export default NewCars;
+    
---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h6>&copy; Designed by Muhammad Rizwan Abbasi</h6>
+      <h6>***92300-2681641***</h6>
+      <h6>***92339-4126816***</h6>
+    </footer>
+  );
+};
+
+export default Footer;
